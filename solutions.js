0afb6eed5d438ec55e2a5271ed2a8d6c3ed2b9bb
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-01 22:27:34 +0100

    Create solutions.js

---
+++ solutions.js
@@ -0,0 +1,6 @@
+// Where my anagrams at?
+function anagrams(word, words) {
+    return words.filter(function(item) {
+        return item.split('').sort().join('') === word.split('').sort().join('');
+    });
+}
