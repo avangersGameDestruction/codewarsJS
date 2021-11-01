60447b93b6230ba4374be3fcfc063ba39c2a4ff2
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-01 23:05:15 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -191,3 +191,20 @@ const calculator = operation => {
 
     return result;
 }
+
+// Directions Reduction
+function dirReduc(plan) {
+    var opposite = {
+        'NORTH': 'SOUTH',
+        'EAST': 'WEST',
+        'SOUTH': 'NORTH',
+        'WEST': 'EAST'
+    };
+    return plan.reduce(function(dirs, dir) {
+        if (dirs[dirs.length - 1] === opposite[dir])
+            dirs.pop();
+        else
+            dirs.push(dir);
+        return dirs;
+    }, []);
+}
