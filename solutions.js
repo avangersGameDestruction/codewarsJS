25ae03d767e00c18419cd482c544bcc796c652ef
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-01 22:41:18 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -121,3 +121,21 @@ function iqTest(numbers) {
 
     return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
 }
+
+// The observed PIN
+function getPINs(observed) {
+    return observed.split('')
+        .map(t => ({
+            '0': ['0', '8'],
+            '1': ['1', '2', '4'],
+            '2': ['1', '2', '3', '5'],
+            '3': ['2', '3', '6'],
+            '4': ['1', '4', '5', '7'],
+            '5': ['2', '4', '5', '6', '8'],
+            '6': ['3', '5', '6', '9'],
+            '7': ['4', '7', '8'],
+            '8': ['5', '7', '8', '9', '0'],
+            '9': ['6', '8', '9']
+        }[t]))
+        .reduce((pre, cur) => [].concat.apply([], pre.map(t => cur.map(g => t + g))));
+}
