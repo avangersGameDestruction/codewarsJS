703e87e8c4b652b82b3469cb96a2d99b88e8e5ea
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-01 22:31:34 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -92,3 +92,14 @@ var isSquare = function(n) {
     }
     if (n < 0) { return false; } else if (a == 1) { return false; } else { return true; }
 }
+
+// Tribonacci Sequence
+function tribonacci(signature, n) {
+    if (n === 0) { return []; } else if (n === 1) { return [1]; } else {
+        for (let i = 3; i <= n; i++) {
+            signature[i] = signature[i - 1] + signature[i - 2] + signature[i - 3];
+        }
+        signature.pop();
+        return signature;
+    }
+}
