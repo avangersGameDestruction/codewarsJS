0ee7c1d583618568f42a6ec983fdfddfeed15688
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-01 22:29:16 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -28,3 +28,21 @@ function order(words) {
         return a.match(/\d/) - b.match(/\d/);
     }).join(' ');
 }
+
+// Which are in?
+function inArray(array1, array2) {
+    const _match = []
+    for (let i = 0; i < array2.length; i++) {
+        for (let x = 0; x < array1.length; x++) {
+            const a1El = array1[x];
+            const a2El = array2[i];
+            if (a2El.includes(a1El)) { _match.push(a1El) }
+        }
+    }
+    return _match.filter((el, i, a) => a.indexOf(el) === i).sort((a, b) => {
+        const _a = a.toLowerCase();
+        const _b = b.toLowerCase();
+        if (_a < _b) { return -1; }
+        return 1;
+    })
+}
