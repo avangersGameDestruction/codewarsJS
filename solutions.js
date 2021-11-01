4df4db026f03a60816ba7d7065dd70dd49c13da0
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-01 22:38:41 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -111,3 +111,13 @@ function humanReadable(seconds) {
         pad(parseInt(seconds / 60 % 60)) + ":" +
         pad(seconds % 60)
 }
+
+// IQ Test
+function iqTest(numbers) {
+    numbers = numbers.split(" ").map(function(el) { return parseInt(el) });
+
+    var odd = numbers.filter(function(el) { return el % 2 === 1 });
+    var even = numbers.filter(function(el) { return el % 2 === 0 });
+
+    return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
+}
