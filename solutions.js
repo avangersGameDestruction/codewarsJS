196f3e201fb343c014c7b53a96a004140870d9ba
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-07 15:30:03 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -214,3 +214,26 @@ function sortArray(array) {
     const odd = array.filter((x) => x % 2).sort((a, b) => a - b);
     return array.map((x) => x % 2 ? odd.shift() : x);
 }
+
+// Permutation
+function permutations(string) {
+    if (string.length <= 1) {
+        return [string];
+    }
+
+    let finalPermutations = permutations(string.substring(1))
+        .reduce((acc, p) => {
+            let charList = p.split('');
+            for (let i = 0; i <= charList.length; i++) {
+                let newPermutation = charList.slice(0, i)
+                    .concat([string[0]])
+                    .concat(charList.slice(i))
+                    .join('');
+                if (!acc.includes(newPermutation)) {
+                    acc.push(newPermutation);
+                }
+            }
+            return acc;
+        }, []);
+    return finalPermutations;
+}
